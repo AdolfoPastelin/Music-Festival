10e825b88b4fc66eb2becbbf80dce759e5b83958
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-10-16 03:12:34 -0500

    css and dev functions added.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const { src, dest, watch } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+/* Compiles SCSS files to CSS file */
+function css(done) {
+	src('src/scss/app.scss') //Identifies the .scss file to compile.
+		.pipe(sass()) // Compile the file.
+		.pipe(dest('build/css')) // Stores the compiled files.
+	done()
+}
+
+/* Add the --watch flag */
+function dev(done) {
+	watch('src/scss/app.scss', css)
+	done()
+}
+
+// run the functions with gulp + functionName
+exports.css = css
+exports.dev = dev

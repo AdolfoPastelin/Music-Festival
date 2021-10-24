46505a873b8e3876b55f8f3a03903ab6135af960
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-10-24 01:49:52 -0500

    selector for all the folders with the scss extesion was added.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'))
 
 /* Compiles SCSS files to CSS file */
 function css(done) {
-	src('src/scss/app.scss') //Identifies the .scss file to compile.
+	src('src/scss/**/*.scss') //Identifies the .scss file to compile.
 		.pipe(sass()) // Compile the file.
 		.pipe(dest('build/css')) // Stores the compiled files.
 	done()
@@ -11,7 +11,7 @@ function css(done) {
 
 /* Add the --watch flag */
 function dev(done) {
-	watch('src/scss/app.scss', css)
+	watch('src/scss/**/*.scss', css)
 	done()
 }
 

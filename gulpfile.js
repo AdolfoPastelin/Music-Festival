fa6b61c2a237228c5bd4d8550da9702c912e2dbe
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-10-24 20:34:28 -0500

    gulp-plumber devDependency added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const plumber = require('gulp-plumber')
 
 /* Compiles SCSS files to CSS file */
 function css(done) {
 	src('src/scss/**/*.scss') //Identifies the .scss file to compile.
+		.pipe(plumber()) //Avoid gulp execution interruption when a compilation error ocurrs.
 		.pipe(sass()) // Compile the file.
 		.pipe(dest('build/css')) // Stores the compiled files.
 	done()

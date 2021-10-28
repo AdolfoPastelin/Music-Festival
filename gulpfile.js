20e8ef4b63cf7b71feabf03aa20a995126b251f3
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-10-28 02:56:39 -0500

    webp conversion functionality and image minification with parallel execution added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
+
+//CSS
 const sass = require('gulp-sass')(require('sass'))
 const plumber = require('gulp-plumber')
+
+//Images
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+
+//* Gulp functions
 
 /* Compiles SCSS files to CSS file */
 function css(done) {
@@ -11,12 +20,35 @@ function css(done) {
 	done()
 }
 
-/* Add the --watch flag */
+/* Add the --watch flag and executes css gulp function */
 function dev(done) {
 	watch('src/scss/**/*.scss', css)
 	done()
 }
 
+function webpVersion(done) {
+	const options = {
+		quality: 50 // quality from 0 to 100 available
+	}
+
+	src('src/img/**/*.{png,jpg}')
+		.pipe(webp(options)) //take the value from the object prop 'quality'
+		.pipe(dest('build/img'))
+	done()
+}
+
+function minifyImages(done) {
+	const options = {
+		optimizationLevel: 3 //options in gulp package documentation
+	}
+
+	src('src/img/**/*.{png,jpg}')
+		.pipe(cache(imagemin(options))) //take the value from the object prop 'optimizationLevel'
+		.pipe(dest('build/img'))
+	done()
+}
+
 // run the functions with gulp + functionName
 exports.css = css
-exports.dev = dev
+exports.img = parallel(minifyImages, webpVersion)
+exports.dev = parallel(css, dev)

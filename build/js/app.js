209aa07cd38f824270bda53fed0f89eed9e16390
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-11-27 12:27:36 -0600

    dark mode toggle classes functionality added.

--- build/js/app.js
+++ build/js/app.js
@@ -111,7 +111,14 @@ function darkModeToggle() {
 
 	// Styles of dark mode
 	const body = document.querySelector('body')
-	const paragraph = document.querySelectorAll('.paragraph')
+	const paragraphs = document.querySelectorAll('.paragraph')
+	const dates = document.querySelectorAll('.date')
+	const days = document.querySelectorAll('.day')
+	const stagesNames = document.querySelectorAll('.stage-name')
+	const packageTitles = document.querySelectorAll('.package-title')
+	const packageItems = document.querySelectorAll('.package-body > ul > li')
+
+	console.log(dates)
 
 	// Dynamic creation of picture -> source x2 -> img
 	if (!body.classList.contains('dark-mode-body')) {
@@ -127,8 +134,34 @@ function darkModeToggle() {
 	// Actions that will happen after clicking the icon.
 	pictureEl.addEventListener('click', () => {
 		body.classList.toggle('dark-mode-body')
-		paragraph.forEach(p => p.classList.toggle('dark-mode-paragraph'))
+		paragraphs.forEach(paragraph => {
+			paragraph.classList.toggle('dark-mode-paragraph')
+		})
 
+		dates.forEach(date => {
+			date.classList.toggle('dark-mode-date')
+			date.classList.toggle('date')
+		})
+
+		days.forEach(day => {
+			day.classList.toggle('dark-mode-day')
+			day.classList.toggle('day')
+		})
+
+		stagesNames.forEach(stageName => {
+			stageName.classList.toggle('dark-mode-stage-name')
+			stageName.classList.toggle('stage-name')
+		})
+
+		packageTitles.forEach(packageTitle => {
+			packageTitle.classList.toggle('dark-mode-package-title')
+		})
+
+		packageItems.forEach(packageItem => {
+			packageItem.classList.toggle('dark-mode-package-item')
+		})
+
+		// Icon toggle
 		if (body.classList.contains('dark-mode-body')) {
 			pictureEl.innerHTML = `
 				<source srcset="build/img/icons/sun-icon.avif" type="image/avif">
